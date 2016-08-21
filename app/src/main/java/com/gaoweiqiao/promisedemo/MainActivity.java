2ecef8f27dc03dd69a139ba2e385d4c2b513983c
gaoweiqiao <gaoweiqiao@qq.com>
2016-08-21 16:06:52 +0800

    init

--- app/src/main/java/com/gaoweiqiao/promisedemo/MainActivity.java
+++ app/src/main/java/com/gaoweiqiao/promisedemo/MainActivity.java
@@ -3,7 +3,6 @@ package com.gaoweiqiao.promisedemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.gaoweiqiao.promise.Promise;
